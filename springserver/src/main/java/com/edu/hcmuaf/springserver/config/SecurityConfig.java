6245de747b2b3733adbe12857b7f1d94b2e9f990
letrongtinh27 <20130440@st.hcmuaf.edu.vn>
2024-06-10 10:12:28 +0700

    second

--- springserver/src/main/java/com/edu/hcmuaf/springserver/config/SecurityConfig.java
+++ springserver/src/main/java/com/edu/hcmuaf/springserver/config/SecurityConfig.java
@@ -35,57 +35,58 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req
+                                .requestMatchers("/login-goolge").permitAll()
 
 //                      Security
-                        .requestMatchers(HttpMethod.GET,"/api/category", "api/locations/all").permitAll()
-//                      Movie
-                        .requestMatchers(HttpMethod.POST, "/api/movies/").hasAnyAuthority("admin")
-                                .requestMatchers(HttpMethod.PUT, "/api/movies/{id}").hasAnyAuthority("admin")
-                                .requestMatchers(HttpMethod.DELETE, "/api/movies/{id}").hasAnyAuthority("admin")
-                                .requestMatchers(HttpMethod.GET, "/api/movies/**").permitAll()
-//                                .requestMatchers(HttpMethod.GET, "/api/movies/{id}").permitAll()
-//                                .requestMatchers(HttpMethod.GET, "/api/movies/search").permitAll()
-//                      Show times
-                        .requestMatchers(HttpMethod.GET, "/api/shows/**").permitAll()
-                                .requestMatchers(HttpMethod.DELETE,"/api/shows/{id}").hasAnyAuthority("admin")
-                                .requestMatchers(HttpMethod.PUT,"/api/shows/{id}").hasAnyAuthority("admin")
-                                .requestMatchers(HttpMethod.POST,"/api/shows/").hasAnyAuthority("admin")
+//                        .requestMatchers(HttpMethod.GET,"/api/category", "api/locations/all").permitAll()
+////                      Movie
+//                        .requestMatchers(HttpMethod.POST, "/api/movies/").hasAnyAuthority("admin")
+//                                .requestMatchers(HttpMethod.PUT, "/api/movies/{id}").hasAnyAuthority("admin")
+//                                .requestMatchers(HttpMethod.DELETE, "/api/movies/{id}").hasAnyAuthority("admin")
+//                                .requestMatchers(HttpMethod.GET, "/api/movies/**").permitAll()
+////                                .requestMatchers(HttpMethod.GET, "/api/movies/{id}").permitAll()
+////                                .requestMatchers(HttpMethod.GET, "/api/movies/search").permitAll()
+////                      Show times
+//                        .requestMatchers(HttpMethod.GET, "/api/shows/**").permitAll()
+//                                .requestMatchers(HttpMethod.DELETE,"/api/shows/{id}").hasAnyAuthority("admin")
+//                                .requestMatchers(HttpMethod.PUT,"/api/shows/{id}").hasAnyAuthority("admin")
+//                                .requestMatchers(HttpMethod.POST,"/api/shows/").hasAnyAuthority("admin")
+//
+////                      Theatre
+//                                .requestMatchers(HttpMethod.GET, "/api/theatres/**").permitAll()
+//                                .requestMatchers(HttpMethod.POST, "/api/theatres/").hasAnyAuthority("admin")
+//                                .requestMatchers(HttpMethod.PUT, "/api/theatres/{id}").hasAnyAuthority("admin")
+//                                .requestMatchers(HttpMethod.DELETE, "/api/theatres/{id}").hasAnyAuthority("admin")
+//
+////                      Ticket
+//                        .requestMatchers(HttpMethod.GET, "/api/tickets/**").hasAnyAuthority("admin")
+//                                .requestMatchers(HttpMethod.GET, "/api/tickets/get/{userId}").hasAnyAuthority("admin", "user")
+//                                .requestMatchers(HttpMethod.POST, "/api/tickets/").hasAnyAuthority("admin")
+//                                .requestMatchers(HttpMethod.PUT, "/api/tickets/{id}").hasAnyAuthority("admin")
+//                                .requestMatchers(HttpMethod.DELETE, "/api/tickets/{id}").hasAnyAuthority("admin")
+////                      User
+//                        .requestMatchers(HttpMethod.GET, "/api/users/profile").permitAll()
+//                                .requestMatchers(HttpMethod.GET, "/api/users/all").hasAnyAuthority("admin")
+//                                .requestMatchers(HttpMethod.GET, "/api/users/{id}").hasAnyAuthority("admin")
+//                                .requestMatchers(HttpMethod.POST, "/api/users/edit").hasAnyAuthority("admin")
+//                                .requestMatchers(HttpMethod.POST, "/api/users/admin_create").hasAnyAuthority("admin")
+//                                .requestMatchers(HttpMethod.DELETE, "/api/users/").hasAnyAuthority("admin")
+////                                .requestMatchers(HttpMethod.GET, "/api/users").hasAnyAuthority("admin")
+//
+//
+////                      Payment
+//                        .requestMatchers(HttpMethod.POST, "/api/payment/pay").hasAnyAuthority("admin","user")
+//                                .requestMatchers(HttpMethod.GET, "/api/payment-callback").hasAnyAuthority("admin", "user")
+////                      Seat
+//                        .requestMatchers(HttpMethod.GET, "/api/seats/get/**").hasAnyAuthority("admin","user")
 
-//                      Theatre
-                                .requestMatchers(HttpMethod.GET, "/api/theatres/**").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/api/theatres/").hasAnyAuthority("admin")
-                                .requestMatchers(HttpMethod.PUT, "/api/theatres/{id}").hasAnyAuthority("admin")
-                                .requestMatchers(HttpMethod.DELETE, "/api/theatres/{id}").hasAnyAuthority("admin")
-
-//                      Ticket
-                        .requestMatchers(HttpMethod.GET, "/api/tickets/**").hasAnyAuthority("admin")
-                                .requestMatchers(HttpMethod.GET, "/api/tickets/get/{userId}").hasAnyAuthority("admin", "user")
-                                .requestMatchers(HttpMethod.POST, "/api/tickets/").hasAnyAuthority("admin")
-                                .requestMatchers(HttpMethod.PUT, "/api/tickets/{id}").hasAnyAuthority("admin")
-                                .requestMatchers(HttpMethod.DELETE, "/api/tickets/{id}").hasAnyAuthority("admin")
-//                      User
-                        .requestMatchers(HttpMethod.GET, "/api/users/profile").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/api/users/all").hasAnyAuthority("admin")
-                                .requestMatchers(HttpMethod.GET, "/api/users/{id}").hasAnyAuthority("admin")
-                                .requestMatchers(HttpMethod.POST, "/api/users/edit").hasAnyAuthority("admin")
-                                .requestMatchers(HttpMethod.POST, "/api/users/admin_create").hasAnyAuthority("admin")
-                                .requestMatchers(HttpMethod.DELETE, "/api/users/").hasAnyAuthority("admin")
-//                                .requestMatchers(HttpMethod.GET, "/api/users").hasAnyAuthority("admin")
-
-
-//                      Payment
-                        .requestMatchers(HttpMethod.POST, "/api/payment/pay").hasAnyAuthority("admin","user")
-                                .requestMatchers(HttpMethod.GET, "/api/payment-callback").hasAnyAuthority("admin", "user")
-//                      Seat
-                        .requestMatchers(HttpMethod.GET, "/api/seats/get/**").hasAnyAuthority("admin","user")
-
-                        .requestMatchers("/api/**").permitAll()
+//                        .requestMatchers("/api/**").permitAll()
 
                         .anyRequest().authenticated()
                 )
-//                .oauth2Login(oauth2 -> oauth2
-//                        .successHandler(new AuthenticationSuccessHandler())
-//                )
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler(new AuthenticationSuccessHandler())
+                )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
